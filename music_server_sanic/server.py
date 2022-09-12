a7814d8182571c60db050d0da728e76ab41893a4
mno <caitryx@outlook.com>
2022-09-12 14:52:46 +0800

    配置tortoise和cors跨域

--- music_server_sanic/server.py
+++ music_server_sanic/server.py
@@ -5,15 +5,31 @@
 version: python 3.9.2
 description: 
 """
+import sanic.response
 from sanic import Sanic
+from sanic_ext import Extend
+from sanic_cors import CORS
+from tortoise.contrib.sanic import register_tortoise
 
 from settings.base import BaseSetting
 
 
 app = Sanic('yin_music', config=BaseSetting())
+
+# 处理生产配置文件
 try:
-    st = __import__('settings.secret', fromlist=('secret',))
-    app.update_config(st.SecretSetting)
+    st = __import__('settings.product', fromlist=('product',))
+    app.update_config(st.ProdSetting)
 except ModuleNotFoundError:
-    # raise Exception('由于secret模块不存在，所以必须完善settings.base下的配置信息')
-    pass
+    print('由于secret模块不存在，所以必须完善settings.base下的配置信息')
+
+# 注册tortoise
+register_tortoise(app, config=app.config.DB_CONFIG)
+
+# 设置跨域
+Extend(app, extensions=[CORS], config={'CORS': False, 'CORS_OPTIONS': app.config.CORS_OPTIONS})
+
+@app.get('/')
+async def test(request):
+    config = app.config
+    return sanic.response.json({})
--- music_server_sanic/settings/base.py
+++ music_server_sanic/settings/base.py
@@ -9,4 +9,28 @@ from sanic.config import Config
 
 
 class BaseSetting(Config):
-    pass
+    # 跨域配置信息
+    CORS_OPTIONS = {
+        "resources": r'/*',
+        "origins": "*",
+        "methods": ["GET", "POST", "HEAD", "OPTIONS", "PUT", "DELETE"]
+    }
+    # 数据库配置信息(tortoise)
+    DB_CONFIG = {
+        'connections': {
+            'default': {
+                'engine': 'tortoise.backends.mysql',
+                'credentials': {
+                    'host': '127.0.0.1',
+                    'port': '33065',
+                    'user': 'root',
+                    'password': '123456',
+                    'database': 'tp_music',
+                }
+            }
+        },
+        'apps': {
+
+        }
+
+    }
